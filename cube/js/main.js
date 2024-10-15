d467716ca672322275f795cf218bac6f3307339b
elena7993 <elenalee7993@gmail.com>
2024-10-15 22:06:48 +0900

    15_10_24

---
+++ cube/js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+  effect: 'cube',
+  cubeEffect: {
+    slideShadows: false,
+  },
+
+  // direction: 'vertical',
+
+  autoplay: {
+    delay: 1000,
+  },
+});
